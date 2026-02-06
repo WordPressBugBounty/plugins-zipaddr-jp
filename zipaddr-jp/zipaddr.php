7f56fcd22bd7a47f79b6762f592f5efeb79f0cea
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 03:41:50 +0000

    1.42

--- zipaddr-jp/zipaddr.php
+++ zipaddr-jp/zipaddr.php
@@ -2,7 +2,8 @@
 if( !defined('ABSPATH') ) exit;
 function zipaddr_jp_change($output, $opt=""){
 	$flds= zipaddrjp_fld(); foreach($flds as $i => $key){$keys=zipaddr_SYS.$key; $$keys="";}
-	$param= unserialize( get_option(zipaddr_DEFINE) ); // get定義情報
+	$param= @unserialize( get_option(zipaddr_DEFINE) ); // get定義情報
+	if( $param === false ) $param= array();
 	foreach((array)$param as $key => $data){
 		$da= ($data=="&nbsp;") ?  $data : htmlspecialchars($data,ENT_QUOTES,'UTF-8');
 		$$key= $da;
@@ -23,6 +24,7 @@ function zipaddr_jp_change($output, $opt=""){
 	$sysid[13]="Forminator,  forminator";
 	$sysid[14]="booking-package,bookingpackage";
 	$sysid[15]="YubinBango,";
+	$sysid[16]="jetformbuilder,jetformbuilder";
 	$sysid[99]="###other###, tricks";
 //
 	$contf7= strpos($output, 'wpcf7-form');       //Contact Form 7
@@ -34,6 +36,7 @@ function zipaddr_jp_change($output, $opt=""){
 	$visufb= strpos($output, 'vfb-form-');        //Visual Forms Builder
 	$woocmb= strpos($output, 'woocommerce-billing'); //Woo Commerce
 	$woocms= strpos($output, 'woocommerce-shipping');//Woo Commerce
+	$woocma= strpos($output, 'woocommerce-address'); //Woo Commerce
 	$welcart=strpos($output, '[zipcode]');        //Welcart
 	$mailpro=strpos($output, 'id="mailformpro"'); //Mailform Pro
 	$mailfor=strpos($output, "mfpc('mailform'");  //Mailform
@@ -42,7 +45,9 @@ function zipaddr_jp_change($output, $opt=""){
 	$formina=strpos($output, 'forminator-label'); //Forminator
 	$bookpak=strpos($output, 'id="booking-package"'); //booking-package
 	$yubingo=strpos($output, 'h-adr');            //YubinBango
+	$jetform=strpos($output, 'class="jet-form-builder'); //jet-form-builder
 	$yubin=  strpos($output, '郵便番号');
+	$idzip=  strpos($output, 'id="zip');
 //フォームの自動判定
 	$sid= "";
 		 if( $yubingo!==false ) $sid= 15;
@@ -53,13 +58,14 @@ function zipaddr_jp_change($output, $opt=""){
 	else if( $wpmem  !==false ){$sid= 5; $sys_dyna="1";}
 	else if( $wpfms  !==false ) $sid= 6;
 	else if( $visufb !==false ) $sid= 7;
-	else if( $woocmb !==false || $woocms!==false ) $sid= 8;
+	else if( $woocmb !==false || $woocms!==false || $woocma!==false ) $sid= 8;
 	else if( $welcart!==false ) $sid= 9;
 	else if( $mailpro!==false || $mailfor!==false) $sid= 10;
 	else if( $snowmon!==false ) $sid= 11;
 	else if( $tieredw!==false ) $sid= 12;
 	else if( $formina!==false ) $sid= 13;
 	else if( $bookpak!==false ){$sid= 14; $sys_dyna="1";}
+	else if( $jetform!==false && $idzip!==false ) $sid= 16;
 	else if( empty($sys_syid))  $sid= 99;
 //
 	if( $sid < 99 ){;}                            //自動判定
@@ -87,39 +93,36 @@ if(isset($_SERVER['HTTPS'])) {$http=(empty($_SERVER['HTTPS'])||$_SERVER['HTTPS']
 		$wk2= strpos($wk,"autozip");
 		if( empty($wk) || $wk2===false ) $lcpath= zipaddr_git.'zipaddr.css'; // 定義がなければ補う
 	 }                                   // 変換の判定開始
-                                         $uls= zipaddr_COM.'js/zipaddr7.js';
- 	 if( $sys_site=="2"||$sys_site=="3") $uls= zipaddr_git.   'zipaddr3.js';
-//se if( $sys_site == "3" )              $uls= zipaddr_git.   'zipaddr30.js';
-else if( $sys_site == "4" )              $uls= zipaddr_git.   'zipaddrx.js';
-else if( $sys_site == "5" )              $uls= zipaddr_git.   'zipaddra.js';
+                            $uls= zipaddr_COM.'js/zipaddr7.js';
+	 if( $sys_site == "2" ) $uls= zipaddr_git.   'zipaddr3.js';
+else if( $sys_site == "3" ) $uls= zipaddr_git.   'zipaddr30.js';
+else if( $sys_site == "4" ) $uls= zipaddr_git.   'zipaddrx.js';
+else if( $sys_site == "5" ) $uls= zipaddr_git.   'zipaddra.js';
 //	$pre=($sys_site=="4") ?  "D." : "ZP.";        // prefix
 	$pre= "ZP.";                                  // prefix
 //モジュール・ファイル生成
 	$js = $jsfile.' src="'.$uls.'?v='.zipaddr_VERS.'"></script>';
 //オプション・パラメータ生成
-	$js.= $jsfile.">function zipaddr_ownb(){" .$pre."dli='".$sys_deli."';";
-//	$js.= empty($ninjaf) ? $pre."wp='1';" : $pre."wp='2';";
-	$js.= $pre."wp='1';";
-	$js.= $pre."uver='".zipaddrjp_suji(get_bloginfo('version'))."';";
-//	$js.= $pre."min=".$sys_keta.";"  .$pre."uver='".get_bloginfo('version')."';";
-//	if( $opt != "" )    $js.= $pre."welcart='1';";
-	if( $sys_tate!="" ) $js.= $pre."top=".    $sys_tate. ";";
-	if( $sys_yoko!="" ) $js.= $pre."left=".   $sys_yoko. ";";
-	if( $sys_pfon!="" ) $js.= $pre."pfon=".   $sys_pfon. ";";
-	if( $sys_sfon!="" ) $js.= $pre."sfon=".   $sys_sfon. ";";
-	if( $sys_focs!="" ) $js.= $pre."focus='". $sys_focs."';";
-	if( $sys_syid!="" ) $js.= $pre."sysid='". $sys_syid."';";
-	if( $sys_plce!="" ) $js.= $pre."holder='".$sys_plce."';";
-	if( $sys_dyna!="" ) $js.= $pre."dyna='".  $sys_dyna."';";
-	if( $sys_site=="3") $js.= $pre."min='7';";
-	if( defined('zipaddr_IDENT') && zipaddr_IDENT == "3" ) $js.= $pre."usces='1';";
-	$js.= '}</script>';
+	$jx= "function zipaddr_ownb(){" .$pre."dli='".$sys_deli."';";
+	$jx.= $pre."wp='1';";
+	$jx.= $pre."uver='".zipaddrjp_suji(get_bloginfo('version'))."';";
+	if( $sys_tate!="" ) $jx.= $pre."top=".    $sys_tate. ";";
+	if( $sys_yoko!="" ) $jx.= $pre."left=".   $sys_yoko. ";";
+	if( $sys_pfon!="" ) $jx.= $pre."pfon=".   $sys_pfon. ";";
+	if( $sys_sfon!="" ) $jx.= $pre."sfon=".   $sys_sfon. ";";
+	if( $sys_focs!="" ) $jx.= $pre."focus='". $sys_focs."';";
+	if( $sys_syid!="" ) $jx.= $pre."sysid='". $sys_syid."';";
+	if( $sys_plce!="" ) $jx.= $pre."holder='".$sys_plce."';";
+	if( $sys_dyna!="" ) $jx.= $pre."dyna='".  $sys_dyna."';";
+	if( $sys_site=="3") $jx.= $pre."min='7';";
+	if( defined('zipaddr_IDENT') && zipaddr_IDENT == "3" ) $jx.= $pre."usces='1';";
+	$jx.= '}';
+	$js.= '<script defer src="data:text/javascript;base64,'.base64_encode($jx).'"></script>';
 //pmファイル生成
 	if( !empty($apid) ){
 		$js.= $jsfile.' src="'.zipaddr_git.$apid.'.js"></script>';
 	}
 //stylesheetファイル生成
-//	if( $sys_site=="2" || $sys_site=="3" ) $js.= '<link rel="stylesheet" href="'.$lcpath.'" />'; // style
 	if( $sys_site=="2" || $sys_site=="3" ) wp_enqueue_style('zipaddr-jp',$lcpath,array(),1.1); // style
 //オウンコード設定パラメータ生成
 //	if( $sys_parm != "" ){
