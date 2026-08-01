e9684a741c71d10896b84e5c40fc100d7979580d
Sean Murphy <sean@iamseanmurphy.com>
2026-08-01 21:09:13 +0000

    1.44

--- zipaddr-jp/admin.php
+++ zipaddr-jp/admin.php
@@ -2,56 +2,38 @@
 if( !defined('ABSPATH') ) exit;
 
 function zipaddr_admin_menu(){                    // 設定メニュー下にサブメニューを追加
-	add_menu_page( 'Zipaddr-JP', 'Zipaddr-JP', 'activate_plugins', zipaddr_KEYS, 'zipaddr_conf');
-	add_submenu_page(zipaddr_KEYS, __('option','zipaddr-jp'),__('option','zipaddr-jp'), 'activate_plugins', zipaddr_KEYS, 'zipaddr_conf');
-}
-function zipaddr_radio($iname,$selected,$table){
-$cdata1=  '<label><input name="{iname}" id="{iname}_{n}" type="radio" value="{key}"{select} />';
-$cdata1.= '{data}</label><br />'."\n";
-	$ans= "";
-	$n= 0;
-	foreach($table as $key => $data) {
-		$select= ($key==$selected) ?  " checked" : "";
-		$n++;
-		$dat= str_replace("{iname}", $iname, $cdata1);
-		$dat= str_replace("{n}",     $n,     $dat);
-		$dat= str_replace("{select}",$select,$dat);
-		$dat= str_replace("{key}",   $key,   $dat);
-		$dat= str_replace("{data}",  $data,  $dat);
-		$ans.= $dat;
-	}
-	return $ans;
+	add_menu_page( 'Zipaddr-JP', 'Zipaddr-JP', 'activate_plugins', ZIPADDR_JP_KEYS, 'zipaddr_conf');
+	add_submenu_page(ZIPADDR_JP_KEYS, __('option','zipaddr-jp'),__('option','zipaddr-jp'), 'activate_plugins', ZIPADDR_JP_KEYS, 'zipaddr_conf');
 }
 function zipaddr_conf(){
-	if( !empty($_GET['page']) && $_GET['page'] == zipaddr_KEYS ){
+	if( !empty($_GET['page']) && $_GET['page'] == ZIPADDR_JP_KEYS ){
 		if( isset($_POST['zipaddr_token']) && !empty($_POST['zipaddr_token']) ){
 			check_admin_referer('zipaddrjp', 'zipaddrjp_def');
 			$array= array();
-			$flds= zipaddrjp_fld();
+			$flds= zipaddr_jp_fld();
 			foreach($flds as $i => $key){
-				$keys= zipaddr_SYS.$key;          // sys_site
+				$keys= ZIPADDR_JP_SYS.$key;          // sys_site
 				$da= isset($_POST[$keys]) ?  sanitize_text_field(wp_unslash($_POST[$keys])) : "";
 				if( $key=="plce" && $da=="&nbsp;" ) {;}
 				else $da= htmlspecialchars($da,ENT_QUOTES,'UTF-8');
 				$array[$keys]= $da;
 				$$keys= $da;
 			}
-			if( $sys_site < "1" || "5" < $sys_site )    $array[zipaddr_SYS.'site']= "4";
-			if( $sys_keta < "5" || "7" < $sys_keta )    $array[zipaddr_SYS.'keta']= "7";
-//			if( $sys_site == "4" )                      $array[zipaddr_SYS.'keta']= "7";
-			if( $sys_site == "2" )                      $array[zipaddr_SYS.'keta']= "5";
-			else                                        $array[zipaddr_SYS.'keta']= "7";
+			if( $sys_site < "1" || "5" < $sys_site )    $array[ZIPADDR_JP_SYS.'site']= "4";
+			if( $sys_keta < "5" || "7" < $sys_keta )    $array[ZIPADDR_JP_SYS.'keta']= "7";
+			if( $sys_site == "2" )                      $array[ZIPADDR_JP_SYS.'keta']= "5";
+			else                                        $array[ZIPADDR_JP_SYS.'keta']= "7";
 			if( $sys_deli=="" || $sys_deli=="-" ){;}
-			else                                        $array[zipaddr_SYS.'deli']= "-";
-			if( !preg_match("/^[0-9\-]+$/",$sys_tate) ) $array[zipaddr_SYS.'tate']= "";
-			if( !preg_match("/^[0-9\-]+$/",$sys_yoko) ) $array[zipaddr_SYS.'yoko']= "";
-			if( !preg_match("/^[0-9\-]+$/",$sys_pfon) ) $array[zipaddr_SYS.'pfon']= "";
-			if( !preg_match("/^[0-9\-]+$/",$sys_sfon) ) $array[zipaddr_SYS.'sfon']= "";
+			else                                        $array[ZIPADDR_JP_SYS.'deli']= "-";
+			if( !preg_match("/^[0-9\-]+$/",$sys_tate) ) $array[ZIPADDR_JP_SYS.'tate']= "";
+			if( !preg_match("/^[0-9\-]+$/",$sys_yoko) ) $array[ZIPADDR_JP_SYS.'yoko']= "";
+			if( !preg_match("/^[0-9\-]+$/",$sys_pfon) ) $array[ZIPADDR_JP_SYS.'pfon']= "";
+			if( !preg_match("/^[0-9\-]+$/",$sys_sfon) ) $array[ZIPADDR_JP_SYS.'sfon']= "";
 			if( $sys_dyna=="" || $sys_dyna=="1" ){;}
-			else                                        $array[zipaddr_SYS.'dyna']= "";
-			$array[zipaddr_SYS.'parm']= str_replace(",", "|", $sys_parm);
+			else                                        $array[ZIPADDR_JP_SYS.'dyna']= "";
+			$array[ZIPADDR_JP_SYS.'parm']= str_replace(",", "|", $sys_parm);
 			$param= serialize($array);
-			update_option(zipaddr_DEFINE,$param); // 定義情報を更新する
+			update_option(ZIPADDR_JP_DEFINE,$param); // 定義情報を更新する
 			$mesg= "稼働環境を設定しました。";
 		}
 		else $mesg= "【稼働環境の設定】";
@@ -76,8 +58,8 @@ function zipaddr_conf(){
 		$comnt[3]= "（@ZipAddr_xx部分）";
 		$rei= "例：";
 //
-		$flds= zipaddrjp_fld(); foreach($flds as $i => $key){$keys=zipaddr_SYS.$key; $$keys="";}
-		$param= unserialize( get_option(zipaddr_DEFINE) ); // get定義情報
+		$flds= zipaddr_jp_fld(); foreach($flds as $i => $key){$keys=ZIPADDR_JP_SYS.$key; $$keys="";}
+		$param= unserialize( get_option(ZIPADDR_JP_DEFINE) ); // get定義情報
 		foreach((array)$param as $key => $data){
 			$da= ($data=="&nbsp;") ?  $data : htmlspecialchars($data,ENT_QUOTES,'UTF-8');
 			$$key= $da;
@@ -104,67 +86,67 @@ function zipaddr_conf(){
 		$serct= array();                          // sys_gide
 		for( $j=0;$j<count($ky2);$j++ ){$serct[$j]= ($ky2[$j] == $sys_gide) ?  "checked " : "";} //利用サイト_radio
 		$param= str_replace("|", ",", $sys_parm);
-		$token= zipaddrjp_str();                  // token
+		$token= zipaddr_jp_str();                 // token
 ?>
-<h2><?php zipaddrjp_e($mesg) ?>(zipaddr-jp)&nbsp;V<?php zipaddrjp_e(zipaddr_VERS) ?></h2>
+<h2><?php echo esc_html($mesg) ?>(zipaddr-jp)&nbsp;V<?php echo esc_html(ZIPADDR_JP_VERS) ?></h2>
 <form id="zipaddr-conf" method="post" action="">
 <table border="1" cellspacing="0" cellpadding="8" summary=" ">
-    <tr><td colspan="2" width="90" bgcolor="#f3f3f3"><?php zipaddrjp_e($title[1]) ?></td>
+    <tr><td colspan="2" width="90" bgcolor="#f3f3f3"><?php echo esc_html($title[1]) ?></td>
     </tr>
-    <tr ><td bgcolor="#f3f3f3"><?php zipaddrjp_e($title[2]) ?><span style="color: #ff0000;">&nbsp;<?php zipaddrjp_e($title[0]) ?></span></td>
+    <tr ><td bgcolor="#f3f3f3"><?php echo esc_html($title[2]) ?><span style="color: #ff0000;">&nbsp;<?php echo esc_html($title[0]) ?></span></td>
 <td>
-<label><input name="sys_site" id="sys_site_1" type="radio" value="4" <?php zipaddrjp_e($selct[0]) ?>/><?php zipaddrjp_e($radio[1]) ?></label><br />
-<label><input name="sys_site" id="sys_site_2" type="radio" value="5" <?php zipaddrjp_e($selct[1]) ?>/><?php zipaddrjp_e($radio[2]) ?></label><br />
-<label><input name="sys_site" id="sys_site_3" type="radio" value="2" <?php zipaddrjp_e($selct[2]) ?>/><?php zipaddrjp_e($radio[3]) ?></label><br />
-<label><input name="sys_site" id="sys_site_4" type="radio" value="3" <?php zipaddrjp_e($selct[3]) ?>/><?php zipaddrjp_e($radio[4]) ?></label><br />
+<label><input name="sys_site" id="sys_site_1" type="radio" value="4" <?php echo esc_html($selct[0]) ?>/><?php echo esc_html($radio[1]) ?></label><br />
+<label><input name="sys_site" id="sys_site_2" type="radio" value="5" <?php echo esc_html($selct[1]) ?>/><?php echo esc_html($radio[2]) ?></label><br />
+<label><input name="sys_site" id="sys_site_3" type="radio" value="2" <?php echo esc_html($selct[2]) ?>/><?php echo esc_html($radio[3]) ?></label><br />
+<label><input name="sys_site" id="sys_site_4" type="radio" value="3" <?php echo esc_html($selct[3]) ?>/><?php echo esc_html($radio[4]) ?></label><br />
 </td>
     </tr>
-    <tr><td bgcolor="#f3f3f3"><?php zipaddrjp_e($title[3]) ?></td>
-        <td><input type="text" name="sys_deli" size="5" maxlength="1" value="<?php zipaddrjp_e($sys_deli) ?>" style="ime-mode:disabled;" />&nbsp;&nbsp;(default: '-')</td>
+    <tr><td bgcolor="#f3f3f3"><?php echo esc_html($title[3]) ?></td>
+        <td><input type="text" name="sys_deli" size="5" maxlength="1" value="<?php echo esc_html($sys_deli) ?>" style="ime-mode:disabled;" />&nbsp;&nbsp;(default: '-')</td>
     </tr>
-    <tr><td bgcolor="#f3f3f3"><?php zipaddrjp_e($title[4]) ?></td>
+    <tr><td bgcolor="#f3f3f3"><?php echo esc_html($title[4]) ?></td>
         <td>
-<?php zipaddrjp_e($comnt[1]) ?>:&nbsp;<input type="text" name="sys_tate" size="5" maxlength="4" value="<?php zipaddrjp_e($sys_tate) ?>" style="ime-mode:disabled;" />&nbsp;&nbsp;(default: 18)<br />
-<?php zipaddrjp_e($comnt[2]) ?>:&nbsp;<input type="text" name="sys_yoko" size="5" maxlength="4" value="<?php zipaddrjp_e($sys_yoko) ?>" style="ime-mode:disabled;" />&nbsp;&nbsp;(default: 22)
+<?php echo esc_html($comnt[1]) ?>:&nbsp;<input type="text" name="sys_tate" size="5" maxlength="4" value="<?php echo esc_html($sys_tate) ?>" style="ime-mode:disabled;" />&nbsp;&nbsp;(default: 18)<br />
+<?php echo esc_html($comnt[2]) ?>:&nbsp;<input type="text" name="sys_yoko" size="5" maxlength="4" value="<?php echo esc_html($sys_yoko) ?>" style="ime-mode:disabled;" />&nbsp;&nbsp;(default: 22)
         </td>
     </tr>
-    <tr><td bgcolor="#f3f3f3"><?php zipaddrjp_e($title[5]) ?></td>
+    <tr><td bgcolor="#f3f3f3"><?php echo esc_html($title[5]) ?></td>
         <td>
-PC:&nbsp;<input type="text" name="sys_pfon" size="5" maxlength="4" value="<?php zipaddrjp_e($sys_pfon) ?>" style="ime-mode:disabled;" />&nbsp;&nbsp;(default: 12)<br />
-SF:&nbsp;<input type="text" name="sys_sfon" size="5" maxlength="4" value="<?php zipaddrjp_e($sys_sfon) ?>" style="ime-mode:disabled;" />&nbsp;&nbsp;(default: 20)
+PC:&nbsp;<input type="text" name="sys_pfon" size="5" maxlength="4" value="<?php echo esc_html($sys_pfon) ?>" style="ime-mode:disabled;" />&nbsp;&nbsp;(default: 12)<br />
+SF:&nbsp;<input type="text" name="sys_sfon" size="5" maxlength="4" value="<?php echo esc_html($sys_sfon) ?>" style="ime-mode:disabled;" />&nbsp;&nbsp;(default: 20)
         </td>
     </tr>
-    <tr ><td bgcolor="#f3f3f3"><?php zipaddrjp_e($title[6]) ?><br /><?php zipaddrjp_e($comnt[3]) ?></td>
+    <tr ><td bgcolor="#f3f3f3"><?php echo esc_html($title[6]) ?><br /><?php echo esc_html($comnt[3]) ?></td>
 <td>
-<label><input name="sys_gide" id="sys_gide_1" type="radio" value=""  <?php zipaddrjp_e($serct[0]) ?>/>表示（default）</label><br />
-<label><input name="sys_gide" id="sys_gide_2" type="radio" value="1" <?php zipaddrjp_e($serct[1]) ?>/>非表示</label><br />
+<label><input name="sys_gide" id="sys_gide_1" type="radio" value=""  <?php echo esc_html($serct[0]) ?>/>表示（default）</label><br />
+<label><input name="sys_gide" id="sys_gide_2" type="radio" value="1" <?php echo esc_html($serct[1]) ?>/>非表示</label><br />
 </td>
     </tr>
-    <tr><td bgcolor="#f3f3f3"><?php zipaddrjp_e($title[7]) ?></td>
-        <td><input type="text" name="sys_plce" size="25" maxlength="50" value="<?php zipaddrjp_e($sys_plce) ?>" /></td>
+    <tr><td bgcolor="#f3f3f3"><?php echo esc_html($title[7]) ?></td>
+        <td><input type="text" name="sys_plce" size="25" maxlength="50" value="<?php echo esc_html($sys_plce) ?>" /></td>
     </tr>
-    <tr><td bgcolor="#f3f3f3"><?php zipaddrjp_e($title[8]) ?></td>
-        <td><input type="text" name="sys_focs" size="25" maxlength="50" value="<?php zipaddrjp_e($sys_focs) ?>" /></td>
+    <tr><td bgcolor="#f3f3f3"><?php echo esc_html($title[8]) ?></td>
+        <td><input type="text" name="sys_focs" size="25" maxlength="50" value="<?php echo esc_html($sys_focs) ?>" /></td>
     </tr>
-    <tr><td bgcolor="#f3f3f3"><?php zipaddrjp_e($title[9]) ?></td>
-        <td><input type="text" name="sys_syid" size="25" maxlength="50" value="<?php zipaddrjp_e($sys_syid) ?>" /></td>
+    <tr><td bgcolor="#f3f3f3"><?php echo esc_html($title[9]) ?></td>
+        <td><input type="text" name="sys_syid" size="25" maxlength="50" value="<?php echo esc_html($sys_syid) ?>" /></td>
     </tr>
-    <tr><td bgcolor="#f3f3f3"><?php zipaddrjp_e($title[10]) ?></td>
-        <td><input type="text" name="sys_dyna" size="25" maxlength="50" value="<?php zipaddrjp_e($sys_dyna) ?>" /></td>
+    <tr><td bgcolor="#f3f3f3"><?php echo esc_html($title[10]) ?></td>
+        <td><input type="text" name="sys_dyna" size="25" maxlength="50" value="<?php echo esc_html($sys_dyna) ?>" /></td>
     </tr>
-    <tr><td bgcolor="#f3f3f3"><?php zipaddrjp_e($title[11]) ?></td>
-        <td><input type="text" name="sys_parm" size="25" maxlength="255" value="<?php zipaddrjp_e($param) ?>" placeholder="<?php zipaddrjp_e($rei) ?>bgc=#3366ff,rtrs=" /></td>
+    <tr><td bgcolor="#f3f3f3"><?php echo esc_html($title[11]) ?></td>
+        <td><input type="text" name="sys_parm" size="25" maxlength="255" value="<?php echo esc_html($param) ?>" placeholder="<?php echo esc_html($rei) ?>bgc=#3366ff,rtrs=" /></td>
     </tr>
-    <tr><td bgcolor="#f3f3f3"><?php zipaddrjp_e($title[12]) ?></td>
-        <td><input type="text" name="sys_drct" size="25" maxlength="50" value="<?php zipaddrjp_e($sys_drct) ?>" placeholder="<?php zipaddrjp_e($rei) ?>/contact" /></td>
+    <tr><td bgcolor="#f3f3f3"><?php echo esc_html($title[12]) ?></td>
+        <td><input type="text" name="sys_drct" size="25" maxlength="50" value="<?php echo esc_html($sys_drct) ?>" placeholder="<?php echo esc_html($rei) ?>/contact" /></td>
     </tr>
 </table>
 <br />
 <?php
 	$footer= array();
 	$footer[1]= "▼郵便番号DBの稼働場所は、次の2系統があります。";
-	$footer[2]= "　商用版サイト： ".zipaddr_COM." 系";
-	$footer[3]= "　有償版サイト： ".zipaddr2COM." 系";
+	$footer[2]= "　商用版サイト： ".ZIPADDR_JP_COM." 系";
+	$footer[3]= "　有償版サイト： ".ZIPADDR_JP_2COM." 系";
 	$footer[4]= "";
 	$footer[5]= "※有償版のご利用には別途、";
 	$footer[6]= "▼[システム拡張AP識別子（'_'区切り）]";
@@ -172,22 +154,22 @@ SF:&nbsp;<input type="text" name="sys_sfon" size="5" maxlength="4" value="<?php 
 	$footer[8]= "利用申請が必要となります。";
 	$value=  "この内容で登録する";
 ?>
-<?php zipaddrjp_e($footer[1]) ?><br />
-<?php zipaddrjp_e($footer[2]) ?><br />
-<?php zipaddrjp_e($footer[3]) ?><br />
+<?php echo esc_html($footer[1]) ?><br />
+<?php echo esc_html($footer[2]) ?><br />
+<?php echo esc_html($footer[3]) ?><br />
 
-<?php zipaddrjp_e($footer[5]) ?><a href="<?php zipaddrjp_e(zipaddr2COM) ?>use/" target="_blank"><?php zipaddrjp_e($footer[8]) ?></a><br />
-<?php zipaddrjp_e($footer[6]) ?><br />
-<?php zipaddrjp_e($footer[7]) ?><br />
+<?php echo esc_html($footer[5]) ?><a href="<?php echo esc_html(ZIPADDR_JP_2COM) ?>use/" target="_blank"><?php echo esc_html($footer[8]) ?></a><br />
+<?php echo esc_html($footer[6]) ?><br />
+<?php echo esc_html($footer[7]) ?><br />
 &nbsp;&nbsp;Contact Form 7/&nbsp;MW WP Forme/&nbsp;Trust Form/&nbsp;Ninja Forms/&nbsp;WP-Members/&nbsp;WPForms<br />
 &nbsp;&nbsp;WooCommerce/&nbsp;Welcart/&nbsp;Mailform Pro/&nbsp;Forminator/&nbsp;TieredWorks/&nbsp;booking-package<br />
 &nbsp;&nbsp;SnowMonkeyForm/&nbsp;Visual Form Builder/&nbsp;Jet Form Builder<br />
 <br />
 <div class="btn-area">
 	<ul><li>
-		<input type="hidden" name="zipaddr_token" value="<?php zipaddrjp_e($token) ?>" />
+		<input type="hidden" name="zipaddr_token" value="<?php echo esc_html($token) ?>" />
 		<?php wp_nonce_field( 'zipaddrjp', 'zipaddrjp_def' ); ?>
-		<input name="submit" id="submit" class="button button-primary" type="submit" value="<?php zipaddrjp_e($value) ?>" />
+		<input name="submit" id="submit" class="button button-primary" type="submit" value="<?php echo esc_html($value) ?>" />
 	</li></ul>
 </div>
 </form>
--- zipaddr-jp/zipaddr.php
+++ zipaddr-jp/zipaddr.php
@@ -1,8 +1,8 @@
 <?php
 if( !defined('ABSPATH') ) exit;
 function zipaddr_jp_change($output, $opt=""){
-	$flds= zipaddrjp_fld(); foreach($flds as $i => $key){$keys=zipaddr_SYS.$key; $$keys="";}
-	$param= @unserialize( get_option(zipaddr_DEFINE) ); // get定義情報
+	$flds= zipaddr_jp_fld(); foreach($flds as $i => $key){$keys=ZIPADDR_JP_SYS.$key; $$keys="";}
+	$param= @unserialize( get_option(ZIPADDR_JP_DEFINE) ); // get定義情報
 	if( $param === false ) $param= array();
 	foreach((array)$param as $key => $data){
 		$da= ($data=="&nbsp;") ?  $data : htmlspecialchars($data,ENT_QUOTES,'UTF-8');
@@ -83,11 +83,10 @@ function zipaddr_jp_change($output, $opt=""){
 
 	$jsfile= '<script type="text/javascript" charset="UTF-8"';
 	$http="http"; $lcpath="";                     // http,  // local_path
-if(isset($_SERVER['HTTPS'])) {$http=(empty($_SERVER['HTTPS'])||$_SERVER['HTTPS']=='off')? 'http':'https';}
+	if(isset($_SERVER['HTTPS'])) {$http=(empty($_SERVER['HTTPS'])||$_SERVER['HTTPS']=='off')? 'http':'https';}
 	$pth= isset($_SERVER['SERVER_NAME']) ?  $http.'://'.sanitize_text_field(wp_unslash($_SERVER['SERVER_NAME'])) : ""; // host用
 	if( empty($sys_site) ) $sys_site= "4";        // パラメータの初期変換
 	if( empty($sys_keta) ) $sys_keta= "7";
-//	if( $sys_site=="1"  || $opt!="" ) $sys_site= "4"; // welcartはzipaddrx.js
 	if( $sys_keta < 5 ||  7 < $sys_keta ) $sys_keta= "7";
 	if( $sys_pfon < 9 || 25 < $sys_pfon ) $sys_pfon= "";
 	if( $sys_sfon < 9 || 25 < $sys_sfon ) $sys_sfon= "";
@@ -95,20 +94,20 @@ if(isset($_SERVER['HTTPS'])) {$http=(empty($_SERVER['HTTPS'])||$_SERVER['HTTPS']
 		$lcpath= $pth.'/css/zipaddr.css';
 		$wk= @file_get_contents($lcpath);
 		$wk2= strpos($wk,"autozip");
-		if( empty($wk) || $wk2===false ) $lcpath= zipaddr_git.'zipaddr.css'; // 定義がなければ補う
+		if( empty($wk) || $wk2===false ) wp_enqueue_style('zipaddr-jp',ZIPADDR_JP_PLUGIN_URL."/zipaddr.css",array(),1.1); // 定義がなければ補う
 	 }                                   // 変換の判定開始
-                           $uls=zipaddr_COM.'js/zipaddr7.js';
-	 if( $sys_site == "2" ) $uls= zipaddr_git. 'zipaddr3.js';
-else if( $sys_site == "3" ) $uls= zipaddr_git. 'zipaddr30.js';
-else if( $sys_site == "4" ) $uls= zipaddr_git. 'zipaddrx.js';
-else if( $sys_site == "5" ) $uls= zipaddr_git. 'zipaddra.js';
+                           $uls=ZIPADDR_JP_COM.'js/zipaddr7.js';
+	 if( $sys_site == "2" ) $uls= ZIPADDR_JP_GIT. 'zipaddr3.js';
+else if( $sys_site == "3" ) $uls= ZIPADDR_JP_GIT. 'zipaddr30.js';
+else if( $sys_site == "4" ) $uls= ZIPADDR_JP_GIT. 'zipaddrx.js';
+else if( $sys_site == "5" ) $uls= ZIPADDR_JP_GIT. 'zipaddra.js';
 	$pre= "ZP.";                                  // prefix
 //モジュール・ファイル生成
-	$js = $jsfile.' src="'.$uls.'?v='.zipaddr_VERS.'"></script>';
+	$js = $jsfile.' src="'.$uls.'?v='.ZIPADDR_JP_VERS.'"></script>';
 //オプション・パラメータ生成
 	$jx= "function zipaddr_ownb(){" .$pre."dli='".$sys_deli."';";
 	$jx.= $pre."wp='1';";
-	$jx.= $pre."uver='".zipaddrjp_suji(get_bloginfo('version'))."';";
+	$jx.= $pre."uver='".zipaddr_jp_suji(get_bloginfo('version'))."';";
 	if( $sys_tate!="" ) $jx.= $pre."top=".    $sys_tate. ";";
 	if( $sys_yoko!="" ) $jx.= $pre."left=".   $sys_yoko. ";";
 	if( $sys_pfon!="" ) $jx.= $pre."pfon=".   $sys_pfon. ";";
@@ -118,15 +117,15 @@ else if( $sys_site == "5" ) $uls= zipaddr_git. 'zipaddra.js';
 	if( $sys_plce!="" ) $jx.= $pre."holder='".$sys_plce."';";
 	if( $sys_dyna!="" ) $jx.= $pre."dyna='".  $sys_dyna."';";
 	if( $sys_site=="3") $jx.= $pre."min='7';";
-	if( defined('zipaddr_IDENT') && zipaddr_IDENT == "3" ) $jx.= $pre."usces='1';";
+	if( defined('ZIPADDR_JP_IDENT') && ZIPADDR_JP_IDENT == "3" ) $jx.= $pre."usces='1';";
 	$jx.= '}';
 	$js.= '<script defer src="data:text/javascript;base64,'.base64_encode($jx).'"></script>';
 //pmファイル生成
 	if( !empty($apid) ){
-		$js.= $jsfile.' src="'.zipaddr_git.$apid.'.js"></script>';
+		$js.= $jsfile.' src="'.ZIPADDR_JP_GIT.$apid.'.js"></script>';
 	}
 //stylesheetファイル生成
-	if( $sys_site=="2" || $sys_site=="3" ) wp_enqueue_style('zipaddr-jp',$lcpath,array(),1.1); // style
+//	if( $sys_site=="2" || $sys_site=="3" ) wp_enqueue_style('zipaddr-jp',$lcpath,array(),1.1); // style
 //オウンコード設定パラメータ生成
 //	if( $sys_parm != "" ){
 		$skip1= "skip=1";
@@ -151,7 +150,7 @@ else if( $sys_site == "5" ) $uls= zipaddr_git. 'zipaddra.js';
 	else
 	if( !empty($woocmk) ){
 		$ans= $output;
-		define('zipaddr_url', $uls);              // 参照用
+		define('ZIPADDR_JP_URL', $uls);           // 参照用
 		add_action('wp_enqueue_scripts', 'zipaddr_jp_scripts');
 	}
 	else
@@ -159,8 +158,8 @@ else if( $sys_site == "5" ) $uls= zipaddr_git. 'zipaddra.js';
 	return $ans;
 }
 function zipaddr_jp_scripts(){
-	wp_enqueue_script('zipaddr-jp-blk', zipaddr_url, array(),get_bloginfo('version'),array('in_footer' => true) );
-	wp_enqueue_script('zipaddr-jp-st2', zipaddr_git.'browsjp_zipaddr.js',array(),'1.4',  array('in_footer' => true) );
-	wp_enqueue_script('zipaddr-jp-st3', zipaddr_git.'woocommerce.js',array(),'1.6',  array('in_footer' => true) );
+	wp_enqueue_script('zipaddr-jp-blk', ZIPADDR_JP_URL, array(),get_bloginfo('version'),array('in_footer' => true) );
+	wp_enqueue_script('zipaddr-jp-st2', ZIPADDR_JP_GIT.'browsjp_zipaddr.js',array(),'1.4',  array('in_footer' => true) );
+	wp_enqueue_script('zipaddr-jp-st3', ZIPADDR_JP_GIT.'woocommerce.js',array(),'1.6',  array('in_footer' => true) );
 }
 ?>
